ea79241e4838993da0c675d9d3d77d640c832a69
Disco Turkey <gelsinon.gwen@gmail.com>
2019-05-05 21:37:45 -0600

    got almost everythign working except displaying the wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,18 +6,26 @@ var wins = 0
 //Losses we want to increase this every [i] that meets the loss conditions
 var losses = 0
 
-//Guesses Left they get 9 total this will decrease with every onkeyup
+//Guesses Left they get 9 total this will decrease with every onkeyup and reset to zero at game end
 var remaining = 9
 
 //Your Guesses so far write each guessed letter to the page 
 
-var wrongGuesses =[]
+var wrongGuesses = []
 // if user hits 9 [i] without computerChoice = userGuess reload page
 
 //generate computer guess
-var computerChoices = ["a","b","c","d","e","f","g","h","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-document.onkeyup = function(event) {
+// vars for displaying the information:
+var directionsText = document.getElementById("directions-text")
+var winsText = document.getElementById("winsText");
+var lossesText = document.getElementById("lossesText");
+var remainingText = document.getElementById("remainingText");
+var wrongGuesses = document.getElementById("wrongGuessesText")
+
+//function starts the game
+document.onkeyup = function (event) {
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
@@ -26,36 +34,47 @@ document.onkeyup = function(event) {
 
     //win scenario
     if (userGuess === computerGuess) {
-        //increase wins by 1
-        //reset game, 
-        //clearing wrong guesses
-    
+        wins++; //increase wins by 1
+        remaining = 0; //reset game, 
+        wrongGuesses = []; //clearing wrong guesses
+
         // still in scenario
-    } else if(remaining>0) {
-        //reduce remaining by 1
+    } else if (remaining > 0 && userGuess !== computerGuess) {
+        remaining = remaining - 1;//reduce remaining by 1
         //push wrong guess into the array
-      
-    //donezo
+
+        //donezo scenario
     } else {
-        //increase losses variable by 1 
-        //reset game
-        //clear wrong guesses
+        losses++;
+        remaining = 0;
+        wrongGuesses = []
     }
 
-    
-    Guess what letter I'm thinking of
-    
-    Wins: (# of times the user has guessed the letter correctly)
-    
-    Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-    
-    Guesses Left: (# of guesses left. This will update)
-    
-    Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-    
-    When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-    
-    When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+    // I like how RPS game hides the directions after you start playing I want to try and do that too
+    directionsText.textContent = "";
+
+    //display results
+    winsText.textContent = "Your wins: " + wins;
+    lossesText.textContent = "Your losses " + losses;
+    remainingText.textContent = "Your remaining guesses " + remaining;
+    wrongGuesses.textContent = "Your guesses so far " + wrongGuesses;
+
+
+}
+    // Goals for the exercise:
+    // Guess what letter I'm thinking of
+
+    // Wins: (# of times the user has guessed the letter correctly)
+
+    // Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+
+    // Guesses Left: (# of guesses left.This will update)
+
+    // Your Guesses So Far: (the specific letters that the user typed.Display these until the user either wins or loses.)
+
+    // When the player wins, increase the Wins counter and start the game over again(without refreshing the page).
+
+    // When the player loses, increase the Losses counter and restart the game without a page refresh(just like when the user wins).
 
 
 
@@ -74,4 +93,3 @@ document.onkeyup = function(event) {
     // else 
     //   {alert("You Lose")}}
 
-}
